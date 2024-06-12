427b4abb03eca0875581d28ca667326e26ebe98a
Chin-Jo <jo.chin@divx.co.jp>
2024-06-12 09:43:30 +0900

    2024/6/11コード完成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
     root to: 'tweets#index'
     resources :tweets
+    resources :tweets do
+      resources :comments, only: :create
+      collection do
+        get 'search'
+      end
+    end
     resources :users, only: :show
   # Defines the root path route ("/")
   # root "posts#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_075408) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_082753) do
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tweets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "text"
     t.text "image"
